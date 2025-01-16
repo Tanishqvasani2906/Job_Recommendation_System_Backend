4179c886af771534b2cb5c16d511b5adcaf44ea8
Tanishqvasani2906 <121330015+Tanishqvasani2906@users.noreply.github.com>
2025-01-16 22:02:27 +0530

    changes in default successful url of security config for google

--- src/main/java/com/example/Job_Recommendation_System/Config/SecurityConfig.java
+++ src/main/java/com/example/Job_Recommendation_System/Config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
 //               FOR THE GOOGLE AUTH BELOW 4 LINES OF CODE
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/oauth2/authorization/google") // Redirect to Google login page
-                        .defaultSuccessUrl("/oauth2/callback/google", true) // Post-login success URL
+                        .defaultSuccessUrl("/dashboard", true) // Post-login success URL
                         .failureUrl("/login?error=true") // Redirect in case of failure
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
